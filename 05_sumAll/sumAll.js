da99fdb2cc93f4725ceb0f304ceb536a716e730b
Evan <evanyou2004@gmail.com>
2023-08-24 19:38:35 -0500

    completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1 , num2) {
 
+    if(num1 < 0 || num2 < 0) return "ERROR";
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    let count = 0;
+    const hi = Math.max(num1,num2);
+    const lo = Math.min(num1,num2);
+    for(let i = lo; i<=hi; i++){
+        count+=i;
+    }
+    return count;
 };
 
 // Do not edit below this line
