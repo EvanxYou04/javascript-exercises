69d9faa65a9bada65ec61a2196c999842c1261a6
Evan <evanyou2004@gmail.com>
2023-08-24 19:26:38 -0500

    completed removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(a, ...nums) {
+    const newArray = []; 
 
+    a.forEach(item => {
+        if(!nums.includes(item)) {
+            newArray.push(item);
+        }
+        
+    });
+    return newArray;
 };
 
 // Do not edit below this line
